ba827ef031257a01d071ecfa1d6ae65a5846b381
kreolsky <zaigraeff@gmail.com>
2020-11-16 20:25:43 +0300

    .

--- example.py
+++ example.py
@@ -10,15 +10,20 @@ page_title = 'documents'
 
 client = gsconfig.GoogleOauth(google_oauth2_token_file_path)
 
-gspread = {'gspread_id': gspread_id, 'page_title': page_title}
-# settings = {
-#     'clothes': '1w03vUeO6SLUlfhkCa86hTbRGsZcynJxyFMONHUmGJp4',
-#     'category': '1ZVIhyfrU2hsSf3ENO0efSAis2zW8FHLa-izi8o6J1oc',
-#     'wardrobe': '1MwRGpqjImvDjBN2ScXFKnitmRll2CBKj3n2YSoolEaM',
-#     }
+# gspread = {'gspread_id': gspread_id, 'page_title': page_title}
+settings = {
+    'clothes': '1w03vUeO6SLUlfhkCa86hTbRGsZcynJxyFMONHUmGJp4',
+    'category': '1ZVIhyfrU2hsSf3ENO0efSAis2zW8FHLa-izi8o6J1oc',
+    'wardrobe': '1MwRGpqjImvDjBN2ScXFKnitmRll2CBKj3n2YSoolEaM',
+    }
 
 # backup = gsconfig.tools.load_source_from_backup('_backup/dev.backup')
-game_config = gsconfig.GameConfig(client, settings = gspread, backup = {})
+
+# Загрузка списка конфигов из гуглодоки. 1я строка должна быть key|value
+# game_config = gsconfig.GameConfig(client, settings = gspread, backup = {})
+
+# Загрузка списка конфигов из словаря
+game_config = gsconfig.GameConfig(client, settings = settings, backup = {})
 
 gsconfig.tools.backup_config(game_config, 'dev.backup', '_backup/')
 gsconfig.tools.save_config_documents(game_config, '_json/')
