03bf005edbd4b583e4d7bd51cfeb6d0838afaac0
zaigraeff <zaigraeff@gmail.com>
2023-12-07 03:22:00 +0300

    prepare to PyPi

--- setup.py
+++ setup.py
@@ -3,48 +3,31 @@
 import os.path
 import re
 import sys
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel')
     sys.exit()
 
-
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
 
-description = 'Google Spreadsheets Python API'
-
-long_description = """
-{index}
-
-License
--------
-MIT
-"""
-
-long_description = long_description.lstrip("\n").format(index=read('docs/index.txt'))
-
+description = 'Game Config Tools over gspread (Google Spreadsheets library).'
 version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
                     read('gsconfig/__init__.py'), re.MULTILINE).group(1)
 
 setup(
     name='gsconfig',
-    packages=['gsconfig'],
+    packages=find_packages(),
     description=description,
-    long_description=long_description,
     version=version,
     author='Serge Zaigraeff',
     author_email='zaigraeff@gmail.com',
     url='https://github.com/kreolsky/gsconfig',
     keywords=['spreadsheets', 'google-spreadsheets', 'game-config'],
     install_requires=['gspread'],
-    python_requires='>=3.6.*',
+    python_requires='>=3.6',
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
